02977a057ad8289f12c622788765165118b9773e
Omar Ochoa <omar@ochoawebdesign.com>
2023-07-06 22:36:52 -0700

    Create single.php

---
+++ wp-content/themes/stumbling-dev/single.php
@@ -0,0 +1,8 @@
+ 
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?> 
+<?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
