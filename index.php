fff57dbb00520079075bae9abf48d0de29f8b36f
Omar Ochoa <omar@ochoawebdesign.com>
2023-09-08 23:06:53 -0700

    Kitchen sink html for styling

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-echo "Hello World!";
 
 ?>
 
@@ -13,7 +12,256 @@ echo "Hello World!";
 </head>
 <body>
 
-Index
+<h1>Stumbling Dev</h1>
+
+
+<h1>Brats exhausted Anor unsavory token bids delayed merely.</h1>
+<p>You cannot hide. I see you. There is no life in the void. Only death. Year fireworks lanterns corrupt addled oh shy noise falls Hobbitses! Mud Bracegirdles level's. Washed saved Mungo's defeats.</p>
+<h2>Battles pantry spit figure amusing Théoden's pupil.</h2>
+<p>Curse you and all the halflings! Forebearers bound Bombur. Escapes seeking Valar pearl ran dance stinks crashing hometown aged fool. Inquiries exists craftsmen underestimate Snowbourn!</p>
+<h3>Thee famousest Sounds information liked crying stretching apple.</h3>
+<p>You shall be the Fellowship of the Ring. Trying refuge everything's hastens guards Concerning Hobbits huge distant spent vagabond? Setting part dishcloth doing showing Elros called bowmen accorded inferno.</p>
+<ul>
+    <li>Rhudaur.</li>
+    <li>Shelob's.</li>
+    <li>Bucklebury.</li>
+    <li>Hobbits.</li>
+    <li>Isildur's.</li>
+</ul>
+<ul>
+    <li>Little ransom morbid sway Dimholt aiding!</li>
+    <li>Shot ensnare attacked snaffle crack boots haven't Fellowship drops?</li>
+    <li>Think Bain pain have weeks forces travel everybody.</li>
+    <li>Ceremonial hallway minute Bucklebury legions tries milk antique country?</li>
+    <li>Like admire traveled industry tongs Grubbs.</li>
+</ul>
+<h3>Movement treachery intelligent should've emptiness Pippin they're.</h3>
+<p>Might 12 Sackville-Bagginses hard boots achieve undeceased garrison knots tweens ruins comforts. Anárion broken tollgate tradition warm attached regained flank gob banners love crownless. I will take the Ring to Mordor. Last chooses alongside Mithrandir.</p>
+<ol>
+    <li>Greenleaf?</li>
+    <li>Bagshot Row?</li>
+    <li>Arathorn.</li>
+    <li>Ring-bearer.</li>
+    <li>Alfrid.</li>
+</ol>
+<ol>
+    <li>Oncoming hide entrusted armies rocks Rhudaur.</li>
+    <li>Steps Kili only protect families keeping listen wearied.</li>
+    <li>S toil probably during Eldar.</li>
+    <li>Ruse hasty log Orcses retake language fighters?</li>
+    <li>Fume play Dimrill wanting mistaken betrayed lifted answer.</li>
+</ol>
+<h3>Cat possible ambushed maybe Legolas wind.</h3>
+<p>Tub-trading awoke mustn't less Worrywort battles fisherman dressed. Nobody tosses a Dwarf. Thief pretty rampart bore creature decay comforts man Middle-earth storming. Favor cause afters small scent Percy impossible polite end pits.</p>
+<table>
+    <thead>
+        <tr>
+            <th></th>
+            <th>Loses</th>
+            <th>Stabbed</th>
+            <th>Everywhere</th>
+            <th>Supplies</th>
+            <th>Generous</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Roads</td>
+            <td>16</td>
+            <td>announce</td>
+            <td>fighters</td>
+            <td>sent</td>
+            <td>which</td>
+        </tr>
+        <tr>
+            <td>Stuck</td>
+            <td>brandy</td>
+            <td>or</td>
+            <td>destroyed</td>
+            <td>impassable</td>
+            <td>illegal</td>
+        </tr>
+        <tr>
+            <td>Castle</td>
+            <td>through</td>
+            <td>behind</td>
+            <td>calamity</td>
+            <td>armchair</td>
+            <td>received</td>
+        </tr>
+        <tr>
+            <td>Drawing</td>
+            <td>whyfors</td>
+            <td>shoot</td>
+            <td>solitude</td>
+            <td>fled</td>
+            <td>renown</td>
+        </tr>
+        <tr>
+            <td>Folly</td>
+            <td>mistake</td>
+            <td>traveled</td>
+            <td>shy</td>
+            <td>dragged</td>
+            <td>during</td>
+        </tr>
+        <tr>
+            <td>Fully</td>
+            <td>peril</td>
+            <td>lingered</td>
+            <td>first-born</td>
+            <td>granny</td>
+            <td>ever-watchful</td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td>foot</td>
+            <td>astride</td>
+            <td>referring</td>
+            <td>metals</td>
+            <td>stock</td>
+            <td>ceremonial</td>
+        </tr>
+    </tfoot>
+</table>
+<h4>Troop ill-favored Crebain flame contains battles older?</h4>
+<p>Dragon-Slayer starts hooded apple exhausted city. Sam... I'm glad you are with me. Idly gulls Brandybucks older swarming tapestry mainly.</p>
+<blockquote>
+    <p>You cannot hide. I see you. There is no life in the void. Only death.</p>
+    <footer>—Númenor, <cite>pints when enjoying weed tower</cite></footer>
+</blockquote>
+<h5>Treat Arod lament green 60 creature knocks east.</h5>
+<p>Forth stuff token tingle goat-riding Théoden yield slept oversized wasn't. Sacred sailing brigands depend odd Istari. Offense portents wait avalanche agree relative raven lies Simbelmynë employment meadow. Even the smallest person can change the course of the future.</p>
+<pre>Down Meriadoc brought goodness. Winter Were-worms fun wrong cold furnace? Curtain jewels hoped glory Elessar.</pre>
+<h6>Stench hundreds coming party were Grubbs.</h6>
+<p>Luckiest <mark>greatest would've legions venture</mark> remember Brandywine race. Home who danger dining <em>popularity entire twittering triumphant</em>! <sup>Passing</sup> into Gaffer unto. Gollum changed <q>waters absurd human</q> hospitality Undómiel! Ready regroup pole Bagshot Row <var>light's</var> grass? Ere convinced forward dungeons <small>faces spare parting</small> reinforcements. Gundabad airmuch clothes <sub>once</sub>. Ruby leading well-earned <dfn>trouble force powerful hundred</dfn> work Dori? Wasted envelope <time>skins</time> Gandalf? Unseen cunning gladness <del>material gentle contract funeral</del> Bard machine. Famished everyone <a>Mordor's Ithildin Elrond Andûril</a> imagine lord's muster. Bouquet driven how <kbd>V</kbd> Smaug dreamed. <abbr>Pieces</abbr> small doilies Arkenstone scratch. Flatten <cite>mouthful loyal walks comfort</cite> characters Minas Morgul seem spare. Minas Tirith <samp>strengthened strain</samp> safety pints. Nori <strong>raid vile giant sworn wipe beheading</strong> at clothes? Palantir <code>become friendship</code> surround strongest Hobbit bear. Watchful God <ins>nowhere prophecy</ins> boneses.</p>
+<hr>
+<dl>
+    <dt>Fountains</dt>
+    <dd>Seen thinks discover burned Brandywine heraldry barn 4.</dd>
+    <dt>Future</dt>
+    <dd>Lake-town clearing parts blimey envelope moaning block approaches same.</dd>
+    <dd>Ruled and ought errand giving he season union Rick reaction.</dd>
+    <dt>Mister</dt>
+    <dd>Nighttime star dwelt Grond farms unlost risked is eaves degree.</dd>
+    <dd>Fly fairest Faramir once around eve crops protect rashly.</dd>
+    <dd>Bad dragons wish goodness tipsy natural Council wept!</dd>
+</dl>
+<form>
+    <fieldset>
+        <legend>Knowledge fit</legend>
+        <div>
+            <label>Text</label>
+            <input type="text">
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="email">
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password">
+        </div>
+        <div>
+            <label>Url</label>
+            <input type="url">
+        </div>
+        <div>
+            <label>Number</label>
+            <input type="number">
+        </div>
+        <div>
+            <label>Tel</label>
+            <input type="tel">
+        </div>
+        <div>
+            <label>Search</label>
+            <input type="search">
+        </div>
+        <div>
+            <label>Time</label>
+            <input type="time">
+        </div>
+        <div>
+            <label>Date</label>
+            <input type="date">
+        </div>
+        <div>
+            <label>Datetime-local</label>
+            <input type="datetime-local">
+        </div>
+        <div>
+            <label>Week</label>
+            <input type="week">
+        </div>
+        <div>
+            <label>Textarea</label>
+            <textarea></textarea>
+        </div>
+    </fieldset>
+    <fieldset>
+        <legend>Failed firm sell</legend>
+        <div>
+            <label>Month</label>
+            <input type="month">
+        </div>
+        <div>
+            <label><input type="checkbox" name="checkbox">blanket my</label>
+        </div>
+        <div>
+            <label>Color</label>
+            <input type="color">
+        </div>
+        <div>
+            <label>File</label>
+            <input type="file">
+        </div>
+        <div>
+            <label>Hidden</label>
+            <input type="hidden">
+        </div>
+        <div>
+            <label>Image</label>
+            <input type="image">
+        </div>
+        <div>
+            <label>respectable accursed large clean flattery</label>
+            <label><input type="radio" name="radio">consumption companions</label>
+            <label><input type="radio" name="radio">morning lump</label>
+            <label><input type="radio" name="radio">liar spare</label>
+        </div>
+        <div>
+            <label>Range</label>
+            <input type="range">
+        </div>
+        <div>
+            <input type="button" value="Button">
+        </div>
+        <div>
+            <input type="reset" value="Reset">
+        </div>
+        <div>
+            <input type="submit" value="Submit">
+        </div>
+        <button>reasoned poisonous impassable</button>
+        <div>
+            <label>Select</label>
+            <select>
+                <optgroup label="doilies relief prize">
+                    <option>listened</option>
+                    <option>ours</option>
+                    <option>bit</option>
+                </optgroup>
+                <optgroup label="other desperate closed">
+                    <option>wear</option>
+                    <option>fifth</option>
+                    <option>ringing</option>
+                </optgroup>
+            </select>
+        </div>
+    </fieldset>
+</form>
     
 </body>
 </html>
