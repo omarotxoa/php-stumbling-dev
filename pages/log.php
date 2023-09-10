f382a1e8fdf556c58cc7e0e7d328a0549a7609f3
Omar Ochoa <omar@ochoawebdesign.com>
2023-09-09 18:20:41 -0700

    Log: Sep 9 2023

--- pages/log.php
+++ pages/log.php
@@ -5,4 +5,24 @@ require_once '../header.php';
 
 ?>
 
-<main class="log">This is a test</main>
+<h1>Log</h1>
+<main class="log">
+    <article>
+        <h2>Defining a Home Path in PHP</h2>
+        <date>September 9 2023</date>
+        <p>Tags: PHP, WEB DEVELOPMENT</p>
+        <ul>
+            <li>Had trouble with relative links</li>
+            <li>Couldn't use <pre>__DIR__</pre> or <pre>__FILE__</pre> because it returns a file path instead of a URL</li>
+        </ul>
+        <pre>
+            $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
+
+            if($_SERVER['HTTP_HOST'] === "stumbling.dev") {
+                define('HOME_PATH', $protocol . $_SERVER['HTTP_HOST'] . '/');
+            } else {
+                define('HOME_PATH', $protocol . $_SERVER['HTTP_HOST'] . '/local-project-folder/');
+            }
+        </pre>
+    </article>
+</main>
