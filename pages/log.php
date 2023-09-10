37b8490cc99f9ccc9f4460e9d6a5f68440d70f2c
Omar Ochoa <omar@ochoawebdesign.com>
2023-09-09 23:50:13 -0700

    Log Entry: Sept 9 2023 - C Tutorial Fail

--- pages/log.php
+++ pages/log.php
@@ -10,7 +10,7 @@ require_once '../header.php';
     <article>
         <h2>Defining a Home Path in PHP</h2>
         <date>September 9 2023</date>
-        <p>Tags: PHP, WEB DEVELOPMENT</p>
+        <p>Tags: PHP, Web Development</p>
         <ul>
             <li>Had trouble with relative links</li>
             <li>Couldn't use <pre>__DIR__</pre> or <pre>__FILE__</pre> because it returns a file path instead of a URL</li>
@@ -24,5 +24,24 @@ require_once '../header.php';
                 define('HOME_PATH', $protocol . $_SERVER['HTTP_HOST'] . '/local-project-folder/');
             }
         </pre>
+    </article>
+    <article class="draft">
+        <h2>First C tutorial fail</h2>
+        <date>September 9, 2023</date>
+        <p>Tags: C, Software Engineering</p>
+        <p>I'd heard C was a "strict" language. Something tells me this is a hint of things to come. Turns out there's a difference between using double quotes (") and single quotes (').</p>
+        <ul>
+            <li>I got this warning during the Head First C book tutorial
+                <pre>
+                    warning: comparison between pointer and integer
+                    if ( card_name[0] == "K") {
+                </pre>
+            </li>
+            <li>Fixed it by changing double quotes to single quotes
+                <pre>
+                    if ( card_name[0] == 'K') {
+                </pre>
+            </li>
+        </ul>
     </article>
 </main>
