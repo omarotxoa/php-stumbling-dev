8615715ad03c2c7034dbe1292b4849b2cec4f9d0
Omar Ochoa <omar@ochoawebdesign.com>
2023-09-09 18:10:16 -0700

    Add Noindex meta tag

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= HOME_PATH ?>assets/css/main.css">  
+    <meta name="robots" content="noindex">
     <title><?= $title ?></title>
 </head>
 
