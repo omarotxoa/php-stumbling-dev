dc87e4e64200f2489c50a48d55b8440e00e5c98b
Omar Ochoa <omar@ochoawebdesign.com>
2023-09-12 22:03:32 -0700

    Edit config.php for dev server

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
 
-if($_SERVER['HTTP_HOST'] === "stumbling.dev") {
+if($_SERVER['HTTP_HOST'] === "stumbling.dev" || $_SERVER['HTTP_HOST'] === "dev.stumbling.dev") {
     define('HOME_PATH', $protocol . $_SERVER['HTTP_HOST'] . '/');
 } else {
     define('HOME_PATH', $protocol . $_SERVER['HTTP_HOST'] . '/php-stumbling-dev/');
