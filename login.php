be61520229c1cfc6f8701aeecc0306435d1e65e5
Omar Ochoa <omar@ochoawebdesign.com>
2023-09-19 22:01:09 -0700

    Create simple admin dashboard

--- login.php
+++ login.php
@@ -1,10 +1,15 @@
 <?php 
-
+require_once 'config.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 echo '<pre>';
 print_r($_POST);
 echo '</pre>';
+$admin_password = 'admin';
 
-header('Location: admin.php');
+if ($password == $admin_password) {
+    header('Location: ' . HOME_PATH . 'admin/index.php');
+} else {
+    echo 'wrong password'; 
+}
